e313ff21b5cdceed2f148052fee34e00622ceabf
2701kai <2701kai@gmail.com>
2025-05-18 22:25:01 +0200

    fix: add import for login confirmation

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import confetti from "canvas-confetti";
 
 export default function Login() {
   const [username, setUsername] = useState("");
